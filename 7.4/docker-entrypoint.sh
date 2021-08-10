47d1cdd5d570b9a1d266222cbd316f1102d991ed
Minh Truong <minh@truong.fi>
2021-08-10 21:45:38 +0300

    Move configuration files to root dir

--- 7.4/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-        set -- /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf "$@"
-fi
-
-exec "$@"
